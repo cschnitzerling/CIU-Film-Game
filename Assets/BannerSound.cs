a0df80fd84f10d0d13c8f161b4d7394c1e034d39
manpat <pat@patsown.com>
2015-05-01 21:04:42 +1000

    Banner BANG

---
+++ Assets/BannerSound.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BannerSound : MonoBehaviour {
+	public AudioClip s;
+
+	public void PlaySound(){
+		AudioSource.PlayClipAtPoint(s, transform.position);
+	}
+}
