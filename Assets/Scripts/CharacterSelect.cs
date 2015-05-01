b1ca02883244febd0bcfb47ff829c7ba68796cba
manpat <pat@patsown.com>
2015-05-01 16:36:18 +1000

    Added keyboard controls + character select menu

---
+++ Assets/Scripts/CharacterSelect.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterSelect : MonoBehaviour {
+	public GameObject[] p1selections;
+	public GameObject[] p2selections;
+
+	private int p1select = 0;
+	private int p2select = 0;
+
+	private bool p1ready = false;
+	private bool p2ready = false;
+
+	private float p1stimeout = 0f;
+	private float p2stimeout = 0f;
+
+	void Start(){
+		UpdateSelection(p1selections, p1select);
+		UpdateSelection(p2selections, p2select);
+	}
+
+	void Update(){
+		if(p1ready && p2ready) return;
+
+		p1stimeout -= Time.deltaTime;
+		p2stimeout -= Time.deltaTime;
+
+		var p1mf = Input.GetAxis("Horizontal0")*100f;
+		var p2mf = Input.GetAxis("Horizontal1")*100f;
+
+		int p1m = (p1mf==0f)?0:Mathf.RoundToInt(p1mf / Mathf.Abs(p1mf));
+		int p2m = (p2mf==0f)?0:Mathf.RoundToInt(p2mf / Mathf.Abs(p2mf));
+
+		if(!p1ready && p1stimeout <= 0f && p1m != 0){
+			p1select = Mathf.Clamp(p1select+p1m, 0, p1selections.Length-1);
+			UpdateSelection(p1selections, p1select);
+			p1stimeout = 0.2f;
+		}
+		if(!p2ready && p2stimeout <= 0f && p2m != 0){
+			p2select = Mathf.Clamp(p2select+p2m, 0, p2selections.Length-1);
+			UpdateSelection(p2selections, p2select);
+			p2stimeout = 0.2f;
+		}
+
+		if(Input.GetButtonDown("Attack0")) p1ready = true;
+		if(Input.GetButtonDown("Attack1")) p2ready = true;
+
+		if(p1ready && p2ready){
+			FindObjectOfType<MainMenu>().StartGame();
+		}
+	}
+
+	void UpdateSelection(GameObject[] ss, int s){
+		for(int i = 0; i < ss.Length; i++){
+			ss[i].GetComponent<Animator>().SetBool("selected", i == s);
+		}
+	}
+}
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,6 +3,29 @@ using System.Collections;
 
 public class MainMenu : MonoBehaviour {
 	public GameObject[] menus;
+	private int currentMenu = 0;
+
+	void Start(){
+		FindObjectOfType<CharacterSelect>().enabled = false;
+	}
+
+	void Update(){
+		if(Input.GetButtonDown("Start")){
+			if(currentMenu == 0){
+				SetMenu(1);
+			}/*else{
+				StartGame();
+			}*/
+		}
+
+		if(Input.GetButtonDown("Back")){
+			if(currentMenu == 1){
+				SetMenu(0);
+			}else{
+				EndGame();
+			}
+		}
+	}
 
 	public void StartGame(){
 		Application.LoadLevel("main");
@@ -14,11 +37,15 @@ public class MainMenu : MonoBehaviour {
 
 	public void SetMenu(int m){
 		for(int i = 0; i < menus.Length; i++){
+			menus[i].SendMessage("SetOpen", i == m);
+			
 			if(i == m){
-				menus[i].SendMessage("SetOpen", true);
-			}else{
-				menus[i].SendMessage("SetOpen", false);
+				currentMenu = m;
 			}
+		}
+
+		if(m == 1){
+			FindObjectOfType<CharacterSelect>().enabled = true;
 		}
 	}
 }
